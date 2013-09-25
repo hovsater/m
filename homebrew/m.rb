61102a7b8d6a8ac6886402d33881bc7b54d7b867
Kevin Sjöberg <kev.sjoberg@gmail.com>
2013-09-25 08:38:05 +0200

    Add Homebrew formula

---
+++ homebrew/m.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class M < Formula
+  homepage 'https://github.com/KevinSjoberg/m'
+  url 'https://github.com/KevinSjoberg/m/archive/v0.0.1.tar.gz'
+  sha1 '24898e60f333c125e19800acbff5e97b1010444d'
+
+  def install
+    (share/'m').install "m"
+  end
+
+  def caveats; <<-EOS.undent
+    Make sure to add the following in your ~/.bashrc.
+
+      source #{share}/m/m
+    EOS
+  end
+end
